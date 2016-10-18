c21c7664dc4f36a51c096ea30c983cd3a0675ca0
daniel-cardinale <soamed690@yahoo.com>
2016-10-18 10:02:17 -0400

    Create C tools for future projects

---
+++ tools/typeTool.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    puts("type\tsize");
+    
+    printf("char\t%zd\n", sizeof(char));
+    printf("short\t%zd\n", sizeof(short));
+    printf("int\t%zd\n", sizeof(int));
+    printf("long\t%zd\n", sizeof(long));
+    printf("long long\t%zd\n", sizeof(long long));
+   
+    printf("long double\t%zd\n", sizeof(long double));
+
+    printf("size_t\t%zd\n", sizeof(size_t)); 
+    printf("void *\t%zd\n", sizeof(void *));
+    printf("func *\t%zd\n", sizeof(void(*)()));
+
+}
