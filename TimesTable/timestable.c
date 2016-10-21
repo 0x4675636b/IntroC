aa9ac8556635e31eaf0587a6eb62cdf83d603a76
daniel-cardinale <soamed690@yahoo.com>
2016-10-21 10:44:54 -0400

    Update printing for fixed width

--- TimesTable/timestable.c
+++ TimesTable/timestable.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int get_width(int max_val);
 int print_table(int column, int row);
-int min_max(int min, int column, int row);
+int min_max(int min, int max);
+
 int main(int argc, char *argv[])
 {
 	int row = 10, column = 10, min, max;
@@ -32,7 +35,7 @@ int main(int argc, char *argv[])
 	} else if(argc == 2){
 		printf("%d", print_table(column, row));
 	} else if (argc == 3) {
-		printf("%d", min_max(min, column, row));
+		printf("%d", min_max(min, max));
 	}
 }	
 int print_table(int column, int row)
@@ -63,45 +66,38 @@ int print_table(int column, int row)
 int min_max(int min, int max)
 {
 	int result;
-	int column = max, row = max;
-	int printwidth = max * max;
+	int max_value = max * max;
 	char print = '*';
-
-	//Idea borrowed found on
-	// http://stackoverflow.com/questions/3068397/finding-the-length-of-an-integer-in-c
-	
-	for  
-
-	
-	printf("%3c", print);
-	// Print first times table row
-	for(int counter = min; counter <= row; ++counter){
+	int print_width = get_width(max_value) + 1;
 		
 
-		printf("%3d", counter);
-		if(counter == row){
+	printf("%*c", print_width, print);
+	// Print first times table row
+	for(int counter = min; counter <= max; ++counter){		
+		printf("%*d", print_width, counter);
+		if(counter == max){
 			printf("\n");
 			}
 		}
-	for(int count = min; count <= row; ++count){
-			printf("%3d", count);
-		for(int count2 = min; count2 <= column; ++count2){
+	for(int count = min; count <= max; ++count){
+			printf("%*d", print_width, count);
+		for(int count2 = min; count2 <= max; ++count2){
 				result = count * count2; 
-				printf("%3d", result);
+				printf("%*d", print_width, result);
 			}
 			printf("\n");
-		}
+	}
 	return 0; 
 }
 int get_width(int max_val)
 {
+	/*Idea and Implementation for determining integer length was found on: 
+http://stackoverflow.com/questions/3068397/finding-the-length-of-an-integer-in-c
+	*/
 	int i = 1;
 	while(max_val){
-		i++
-		value /= 10;
-		return i;
-	}
-
-
-
+		i++;
+		max_val /= 10;
+		
+	} return i;
 }
