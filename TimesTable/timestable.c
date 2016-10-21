6311dffb9bea5089ff6fb5dbd4c7b6ff85a138f8
daniel-cardinale <soamed690@yahoo.com>
2016-10-21 11:15:53 -0400

    Update variables being used

--- TimesTable/timestable.c
+++ TimesTable/timestable.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int get_width(int max_val);
 int print_table(int column, int row);
-int min_max(int min, int max);
+int min_max(int row, int column);
 
 int main(int argc, char *argv[])
 {
-	int row = 10, column = 10, min, max;
+	int row = 10, column = 10;
 	
-
 	if(argc > 3) {
 		printf("Usage %s <value> \n", argv[0]);
 		return 1;
@@ -18,8 +16,8 @@ int main(int argc, char *argv[])
 		column = strtol(argv[1], NULL, 10);
 		row = strtol(argv[1],NULL, 10);
 	} else if(argc == 3){
-		max = strtol(argv[2], NULL, 10);
-		min = strtol(argv[1], NULL, 10);
+		column = strtol(argv[2], NULL, 10);
+		row = strtol(argv[1], NULL, 10);
 	}
 
 	if((row > 100) ||  (row <= 0)){
@@ -28,21 +26,13 @@ int main(int argc, char *argv[])
 	} else if ((column > 100) || (column <= 0)){
 		printf("Usage %s <value> \n", argv[0]);
 		return 1;
-	} 
-	if(max > 100 || max < min){
-		printf("Usage %s <value> \n", argv[0]);
-		return 1;
 	}
-
-
-
-	
 	if(argc == 1){
 		printf("%d", print_table(column, row));
 	} else if(argc == 2){
 		printf("%d", print_table(column, row));
-	} else if (argc == 3) {
-		printf("%d", min_max(min, max));
+	} else if (argc == 3){
+		printf("%d", min_max(row, column));
 	}
 }	
 int print_table(int column, int row)
@@ -56,8 +46,8 @@ int print_table(int column, int row)
 		printf("%4d", count);
 		if(count == row){
 			printf("\n");
-			}
 		}
+	}
 		
 	for(int count = 1; count <= row; ++count){
 			printf("%4d", count);
@@ -65,33 +55,33 @@ int print_table(int column, int row)
 				result = count * count2; 
 				printf("%4d", result);
 		
-			}
-			printf("\n");
 		}
+			printf("\n");
+	}
 	return 0;
 }
-int min_max(int min, int max)
+int min_max(int row,  int column)
 {
 	int result;
-	int max_value = max * max;
+	int max_value = column * column;
 	char print = '*';
 	int print_width = get_width(max_value) + 1;
 		
 
 	printf("%*c", print_width, print);
 	// Print first times table row
-	for(int counter = min; counter <= max; ++counter){		
+	for(int counter = row; counter <= column; ++counter){		
 		printf("%*d", print_width, counter);
-		if(counter == max){
+		if(counter == column){
 			printf("\n");
-			}
 		}
-	for(int count = min; count <= max; ++count){
+	}
+	for(int count = row; count <= column; ++count){
 			printf("%*d", print_width, count);
-		for(int count2 = min; count2 <= max; ++count2){
+		for(int count2 = row; count2 <= column; ++count2){
 				result = count * count2; 
 				printf("%*d", print_width, result);
-			}
+		}
 			printf("\n");
 	}
 	return 0; 
@@ -106,5 +96,6 @@ http://stackoverflow.com/questions/3068397/finding-the-length-of-an-integer-in-c
 		i++;
 		max_val /= 10;
 		
-	} return i;
+	} 
+	return i;
 }
