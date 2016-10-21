53c8ab2b994ad9136ac59bcb95b6bad0c5f17330
daniel-cardinale <soamed690@yahoo.com>
2016-10-20 22:46:29 -0400

    Update to times table in order to add functions

--- TimesTable/timestable.c
+++ TimesTable/timestable.c
@@ -1,44 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int print_table(int column, int row);
+
 int main(int argc, char *argv[])
 {
-	int row, column, result;
+	int row = 10, column = 10;
+	
+	printf("%d", print_table(column, row));
+
+	if(argc != 2) {
+		printf("Usage %s <value> \n", argv[0]);
+		return 1;
+	} else if(argc == 1) {
+		column = strtol(argv[1], NULL, 10);
+		row = strtol(argv[1], NULL, 10);
+		return 1;
+	}
+	
+	/*if((row > 100) ||  (row < 0)){
+		printf("Usage %s <value> \n", argv[0]);
+		
+	} else if ((column > 100) || (column < 0)){
+		printf("Usage %s <value \n", argv[0]);
+		
+	}*/
+}	
+int print_table(int column, int row)
+{	
+	int result;		
 	char print = '*';
 	
-	if(argc > 2){
-		printf("Usage %s <value> \n", argv[0]);
-	} else if(argc == 1 &&strtol(argv[1], NULL, 10) <= 100 && strtol(argv[1], NULL, 10) > 0){
-
-		row = strtol(argv[1], NULL, 10);
-		column = strtol(argv[1], NULL, 10);
-	
-	 } else {
-
-		row = 10;
-		column = 10;
-	}
-
 	printf("%3c", print);
 	// Print first times table row
 	for(int count = 1; count <= row; ++count){
 		printf("%5d", count);
-		if(count == 10){
+		if(count == row){
 			printf("\n");
+			}
 		}
-	}
 		
 	for(int count = 1; count <= row; ++count){
 			printf("%3d", count);
 		for(int count2 = 1; count2 <= column; ++count2){
-				
 				result = count * count2; 
 				printf("%5d", result);
 		
+			}
+			printf("\n");
 		}
-		printf("\n");
-	}
-
-
-
+	return 0;
 }
